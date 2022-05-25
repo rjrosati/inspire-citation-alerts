bb6450e6d9cfa0cf26b4df836fdf3fb7f6aa8cb7
Robert Rosati <robert.j.rosati@nasa.gov>
2022-05-25 17:03:33 -0500

    update date

--- citation_query.py
+++ citation_query.py
@@ -52,13 +52,13 @@ queries_to_make = {
         'Mateo Braglia': 'M.Braglia.1',
         'Vikas Aragam': 'V.Aragam.1',
         }
-lastrun = datetime(2022,1,18)
+lastrun = datetime(2022,5,25)
 
 def process_json_date(datestr):
     try:
         return datetime.strptime(datestr, "%Y-%m-%d")
     except:
-        try: 
+        try:
             return datetime.strptime(datestr, "%Y-%m")
         except:
             return datetime.strptime(datestr, "%Y")
